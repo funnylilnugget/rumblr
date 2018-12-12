800e8fc610f17cb33a5c75598ebfeea0ebaa942c
funnylilnugget <kenny@funnylilnugget.com>
2018-12-11 19:15:54 -0500

    created rumblr project, added sinatra code, various erb files

---
+++ server.rb
@@ -0,0 +1,88 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+
+enable :sessions
+
+set :database, {adapter: "sqlite3", database: "article.sqlite3"}
+
+class User < ActiveRecord::Base
+end
+
+class Blog < ActiveRecord::Base
+end
+
+get '/' do
+  p session
+  erb :home
+end
+
+# BLOGS
+
+get "/blogs/blog" do
+  if session['user_id'] == nil
+    p 'User was not logged in'
+    redirect '/'
+  end
+  erb :"blogs/blog"
+end
+
+post "/blogs/blog" do
+  @blog = Blog.new(title: params[:title], content: params[:content], user_id: params[:user_id])
+  @blog.save
+  redirect "/blog/#{@blog.id}"
+end
+
+get "/blogs/:id" do
+  @blog =  Blog.find(params["id"])
+  erb :"/blogs/allblogs"
+end
+
+# LOG IN PAGE
+
+get '/login' do
+  erb :'/users/login'
+end
+
+post '/login' do
+  p params
+  user = User.find_by(email: params['email'])
+  if user != nil
+    if user.password == params['password']
+      session[:user_id] = user.id
+      redirect "/users/#{user.id}"
+    end
+  end
+end
+
+# LOG OUT
+
+post "/logout" do
+
+  session["user_id"] = nil
+  redirect "/"
+end
+
+# SIGN UP PAGE
+
+get "/users/signup" do
+  if session['user_id'] != nil
+    p "User already logged in"
+    redirect "/"
+  else
+    erb :'/users/signup'
+  end
+end
+
+post "/users/signup" do
+  @user =  User.new(first_name: params[:first_name],last_name: params[:last_name], email: params[:email], birthday: params[:birthday], password: params[:password])
+  @user.save
+  session[:user_id] = @user.id
+  redirect "/users/#{@user.id}"
+end
+
+# PROFILE
+
+get "/users/:id" do
+  @user =  User.find(params["id"])
+  erb :"/users/profile"
+end
